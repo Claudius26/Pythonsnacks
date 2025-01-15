33d8c7835a55cb9b75343d5bb188937bac2cb135
Claudius26 <osomzed@gmail.com>
2025-01-15 05:11:16 -0800

    Try this

--- PhaseGate1/sum_of_digit.py
+++ PhaseGate1/sum_of_digit.py
@@ -1,8 +1,20 @@
 number = int(input("Enter a number between 0-1000: "))
 
+while number < 0 or number > 1000:
 
+	print('Invalid number')
 
-for _ in 'family':
+	number = int(input("Enter a number between 0-1000: "))
 
-	print(_)
+total = 0
+
+while number > 0:
+
+	digit_number = number % 10
+
+	total += digit_number
+
+	number //= 10
+
+print(total)
 
