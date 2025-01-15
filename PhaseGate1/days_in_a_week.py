5a997d3e7b041119758324f93579c52805262a54
Claudius26 <osomzed@gmail.com>
2025-01-15 06:06:26 -0800

    add it

--- PhaseGate1/days_in_a_week.py
+++ PhaseGate1/days_in_a_week.py
@@ -14,15 +14,15 @@ while future_day < 0 or future_day > (len(days_of_the_week) - 1):
 
 	print('Invalid input')
 
-	first_day = int(input("Enter todays day: "))
+	future_day = int(input("Enter the number of days elapsed since today: "))
 
 sum_future_days = first_day + future_day
 
 if sum_future_days > len(days_of_the_week):
 
-	difference = sum_future_days % first_day
+	difference = sum_future_days % 7
 
-	sum_future_days = difference - 2	
+	sum_future_days = difference	
 
 print('Today is ', days_of_the_week[first_day], 'and the future day is ', days_of_the_week[sum_future_days])
 
