25797567c6dc45ba594f17d07310849f3f743dc5
Claudius26 <osomzed@gmail.com>
2025-03-05 07:59:05 +0100

    add it

--- TestMovieRatingFunction.py
+++ TestMovieRatingFunction.py
@@ -8,9 +8,40 @@ class TestMovieRatingFunction(unittest.TestCase):
 
 	def test_that_movie_rating_can_add_movie(self):
 
-		actual = movie_rating.add_movie({"movie": "merlin"})
+		actual = movie_rating.add_movie("merlin")
 
-		expected = {"movie": "merlin" }
+		expected = {"movie": "merlin", "Date Added": actual["Date Added"] }
 
 		self.assertEqual(actual, expected)
 
+	def test_that_movie_rating_can_add_correct_date_and_time(self):
+
+		actual = movie_rating.add_movie("merlin")
+
+		expected = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+		self.assertEqual(actual["Date Added"], expected)
+
+	def test_that_movie_rating_can_add_movie_returns_invalid_input_when_wrong_data_is_inputed(self):
+
+		actual = movie_rating.add_movie(1)
+
+		expected = "Invalid input"
+
+		self.assertEqual(actual, expected)
+
+	def test_that_function_can_rate_movie(self):
+
+		actual = movie_rating.add_ratings(2.0)
+
+		expected = 2.0
+
+		self.assertEqual(actual, expected)
+
+	def test_that_function_can_rate_returns_invalid_rating_if_rating_is_out_of_range(self):
+
+		actual = movie_rating.add_ratings(5.1)
+
+		expected = "Invalid rating"
+
+		self.assertEqual(actual, expected)
--- movie_rating.py
+++ movie_rating.py
@@ -1,8 +1,22 @@
+from datetime import datetime
 list_of_movies = {"names_of_movies": {}}
 
 def add_movie(name):
 
+	if type(name) != str:
 
-	list_of_movies["names_of_movies"][name] = {"movie": name}
+		return "Invalid input"
+
+	time_added = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+	list_of_movies["names_of_movies"][name] = {"movie": name, "Date Added": time_added}
 
 	return list_of_movies["names_of_movies"][name]
+
+def add_ratings(rating):
+
+	if rating < 1 or rating > 5:
+
+		return "Invalid rating"
+
+	return rating
